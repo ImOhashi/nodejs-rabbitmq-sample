f3d10bb43697b4b1a94e5c352b00fcc3b4d9ba55
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-03-28 00:05:44 -0300

    add a new publish

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,6 +15,12 @@ class Routes {
       try {
         await rabbit.publishInQueue(JSON.stringify(req.body));
 
+        await rabbit.publishInExchange(
+          "amq.direct",
+          "rota",
+          JSON.stringify(req.body)
+        );
+
         res.status(200).send(req.body);
       } catch (error) {
         res.status(500).send(error);
